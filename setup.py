ad5d50495fe06c3325219728854b632fbad59475
mon4oy <simeonp@delasport.com>
2016-11-02 11:46:16 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='odds_calculator',
+setup(name='oddscalculator',
       version='0.1',
       description='Calculator for decimal, malay, american, hongkong, indonesian odds',
       url='https://github.com/simopopov/odds_calculator.git',
       author='Simeon Popov',
       author_email='simopopov@gmail.com',
       license='MIT',
-      packages=['odds_calculator'],
+      packages=['oddscalculator'],
       zip_safe=False)
